d752033e4af1993596306f73ac4d7450f820d7ad
Edu Rus <edamamon@gmail.com>
2023-02-28 11:53:54 +0100

    rails generate scaffold
    category (categorias o descriociones
    name: (tipos que va a tener)
    string (y como son esos tipos)
     genera migracion que nos quedaria pdte

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_105030) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_104929) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,10 +39,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_105030) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "description", null: false
-    t.integer "price", null: false
+    t.string "title"
+    t.text "description"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
