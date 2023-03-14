b0e9e1bfe336235dc23bbd1bf012486a30f5e7e1
Edu Rus <edamamon@gmail.com>
2023-03-14 10:23:52 +0100

    Añadir favoritos

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,11 @@ class FavoritesController < ApplicationController
        redirect_to product_path(product)
     end
 
+    def destroy
+       product.favorites.find_by(user: Current.user).destroy
+       redirect_to product_path(product) , status: :see_other
+    end
+
     private
 
     def product
